fbcd4c1e9fb5a64468fd89c595c4df80a39cb36e
hizlp <187222@163.com>
2017-07-11 22:04:18 +0800

    添加编辑的最后完善，模板竟然忘记{{}}，表单的提交注意一下

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -7,9 +7,9 @@ define(['jquery', 'template', 'util'], function ($, template, util) {
     /*编辑讲师信息*/
     /*根据讲师id才能编辑对应信息.封装一个方法去获取讲师的id号(获取地址栏中指定的参数值)。我们在编辑按钮使用拼接字符串方法*/
 
-        /*有tc_id的时候才是编辑功能，才执行下面代码*/
-        var tcId = util.qs('tc_id', location.search);
-        // console.log(tcId);
+    /*有tc_id的时候才是编辑功能，才执行下面代码*/
+    var tcId = util.qs('tc_id', location.search);
+    // console.log(tcId);
     if (tcId) {
 
         $.ajax({
@@ -29,23 +29,47 @@ define(['jquery', 'template', 'util'], function ($, template, util) {
 
                 $('#teacherInfo').html(html);
 
+                $('#swcbtn').click(function () {
+                    /*因为按钮是渲染出来的，所以应该是等有数据了才能点击按钮提交*/
+                    /*编辑时提交----更新数据发送给服务器，但是必须得知道是更新谁的数据，所以页面中要添加一个tc_id*/
+                    submitForm('/api/teacher/update');
+                    /*模板中竟然忘记写{{}}*/
+                });
+
             }
         });
-    }else{
-                /*没有id好就是添加功能*/
-                var html=template('teacherTpl',{tc_operator:'添加讲师',tc_gender:0});
-                /*添加默认文字/选项*/
-                $('#teacherInfo').html(html);
+    } else {
+        /*没有id好就是添加功能*/
+        var html = template('teacherTpl', { tc_operator: '添加讲师', tc_gender: 0 });
+        /*添加默认文字/选项*/
+        $('#teacherInfo').html(html);
+
+        $('#swcbtn').click(function () {
+            /*添加时提交--*/
+            submitForm('/api/teacher/add')
+        });
     }
 
 
-    /*添加讲师信息*/
-    $('#swcbtn').click(function () {
+    // /*绑定表单提交事件*/
+    // $('#swcbtn').click(function () {
+    //     /*因为按钮是渲染出来的，所以应该是等有数据了才能点击按钮提交*/
+    //     if (tcId) {
+    //         /*编辑时提交----更新数据*/
+    //         submitForm('/api/teacher/update');
+    //     } else {
+    //         /*添加时提交*/
+    //         submitForm('/api/teacher/add')
+    //     }
+    // });
+
+
+    function submitForm(url) {
         console.log($('#addForm').serialize());
         /*ajax将数据发送给服务器*/
         $.ajax({
             type: 'post',
-            url: '/api/teacher/add',
+            url: url,
             data: $('#addForm').serialize(),
             dataType: 'json',
             success: function (data) {
@@ -56,8 +80,8 @@ define(['jquery', 'template', 'util'], function ($, template, util) {
                 }
             }
         });
-    });
 
+    }
 
 
 });
